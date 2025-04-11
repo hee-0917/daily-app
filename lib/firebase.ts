53ee359afbc84e2d69b41f48f1edf0ca48bd949a
hee-0917 <nymph4276@nate.com>
2025-04-11 19:40:47 +0900

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // Firestore 추가
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // Firestore 초기화
+
+export { app, db }; // db 내보내기 추가
